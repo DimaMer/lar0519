c446fbd47bf7f6867e77f38e3d26f04e9b1b83d1
DimaMer <dimm1simm1@gmail.com>
2019-05-14 12:05:40 +0300

    smoll

--- resources/views/job/temp.blade.php
+++ resources/views/job/temp.blade.php
@@ -2,31 +2,31 @@
 
 {!! Form::open()!!}
 <div style=" width: 500px; text-align: right; font-size: 24px;">
-{!! Form::label('Jobs count') !!}
-{!! Form::number('name', '1') !!}
-<BR>
+    {!! Form::label('Jobs count') !!}
+    {!! Form::number('name', '1') !!}
+    <BR>
 </div>
 <BR>
-    <h1>Select, which class do you want to parse? </h1>
-    <BR>
-   
+<h1>Select, which class do you want to parse? </h1>
+<BR>
+
 <div style=" width: 500px; text-align: right; font-size: 24px;">
-{!! Form::label('vacancy-title') !!}
-{!!Form::checkbox('class[vacancy]','vacancy-title') !!}<BR>
-{!! Form::label('company-title') !!}
-{!!Form::checkbox("class[company]" ,'company-title') !!}<BR>
-{!! Form::label('time-passed') !!}
-{!!Form::checkbox("class[time]",'time-passed') !!}<BR>
-{!! Form::label('vacancy-info-wrapper vacancy-block post') !!}
-{!!Form::checkbox("class[vacancyInfoWrapper]",'vacancy-info-wrapper vacancy-block post') !!}<BR>
-{!! Form::label('company-description vacancy-block post') !!}
-{!!Form::checkbox("class[companyDescription]" ,'company-description vacancy-block post') !!}<BR>
-{!! Form::label('selected-category') !!}
-{!!Form::checkbox("class[category]",'selected-category') !!}<BR>
-{!! Form::label('selected-city vacancy-city') !!}
-{!!Form::checkbox("class[cityVacancyCity]",'selected-city vacancy-city') !!}<BR>
+    {!! Form::label('(vacancy-title)') !!}
+    {!!Form::checkbox('class[vacancy]','vacancy-title','true') !!}<BR>
+    {!! Form::label('(company-title)') !!}
+    {!!Form::checkbox("class[company]" ,'company-title','true') !!}<BR>
+    {!! Form::label('(time-passed)') !!}
+    {!!Form::checkbox("class[time]",'time-passed','true') !!}<BR>
+    {!! Form::label('(vacancy-info-wrapper)(vacancy-block)(post)') !!}
+    {!!Form::checkbox("class[vacancyInfoWrapper]",'vacancy-info-wrapper vacancy-block post','true') !!}<BR>
+    {!! Form::label('(company-description)(vacancy-block post)') !!}
+    {!!Form::checkbox("class[companyDescription]" ,'company-description vacancy-block post','true') !!}<BR>
+    {!! Form::label('(selected-category)') !!}
+    {!!Form::checkbox("class[category]",'selected-category','true') !!}<BR>
+    {!! Form::label('(selected-city)(vacancy-city)') !!}
+    {!!Form::checkbox("class[cityVacancyCity]",'selected-city vacancy-city','true') !!}<BR>
     <BR>
-{!! Form::submit() !!}
+    {!! Form::submit() !!}
 
 </div>
 {!! Form::close()!!}
--- resources/views/job/viewTemp.blade.php
+++ resources/views/job/viewTemp.blade.php
@@ -8,19 +8,22 @@
     <title>Document</title>
 </head>
 <body>
+{!! Form::open(['route' => 'ParseController@index']) !!}
+{!! Form::submit('Submit', ['class' => 'btn btn-info']) !!}
+{!! Form::submit() !!}
 
 {{print_r($index)}}
 
-    @foreach ($dataAfterSearch as $key=>$value) {
-        <h1>{{$key}}</h1>
-    @foreach ($value as $key1=>$value1) {
-                <h2>{{$key1}}</h2>
-                {{$value1}}
-                     <BR>
+@foreach ($dataAfterSearch as $key=>$value) {
+<h1>{{$key}}</h1>
+@foreach ($value as $key1=>$value1) {
+<h2>{{$key1}}</h2>
+{{$value1}}
+<BR>
 
 
-    @endforeach
-    @endforeach
+@endforeach
+@endforeach
 
 
 
