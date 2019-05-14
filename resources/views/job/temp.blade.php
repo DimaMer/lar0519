67292c14eec2f26a31a87331e007aa4ab87ef068
DimaMer <dimm1simm1@gmail.com>
2019-05-14 03:24:01 +0300

    first commi2t1

---
+++ resources/views/job/temp.blade.php
@@ -0,0 +1,33 @@
+<h1>Write, how many Jobs do you want to parse? </h1>
+
+{!! Form::open()!!}
+<div style=" width: 500px; text-align: right; font-size: 24px;">
+{!! Form::label('Jobs count') !!}
+{!! Form::number('name', '1') !!}
+<BR>
+</div>
+<BR>
+    <h1>Select, which class do you want to parse? </h1>
+    <BR>
+   
+<div style=" width: 500px; text-align: right; font-size: 24px;">
+{!! Form::label('vacancy-title') !!}
+{!!Form::checkbox('class[vacancy]','vacancy-title') !!}<BR>
+{!! Form::label('company-title') !!}
+{!!Form::checkbox("class[company]" ,'company-title') !!}<BR>
+{!! Form::label('time-passed') !!}
+{!!Form::checkbox("class[time]",'time-passed') !!}<BR>
+{!! Form::label('vacancy-info-wrapper vacancy-block post') !!}
+{!!Form::checkbox("class[vacancyInfoWrapper]",'vacancy-info-wrapper vacancy-block post') !!}<BR>
+{!! Form::label('company-description vacancy-block post') !!}
+{!!Form::checkbox("class[companyDescription]" ,'company-description vacancy-block post') !!}<BR>
+{!! Form::label('selected-category') !!}
+{!!Form::checkbox("class[category]",'selected-category') !!}<BR>
+{!! Form::label('selected-city vacancy-city') !!}
+{!!Form::checkbox("class[cityVacancyCity]",'selected-city vacancy-city') !!}<BR>
+    <BR>
+{!! Form::submit() !!}
+
+</div>
+{!! Form::close()!!}
+
---
+++ resources/views/job/viewTemp.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+{{print_r($index)}}
+
+    @foreach ($dataAfterSearch as $key=>$value) {
+        <h1>{{$key}}</h1>
+    @foreach ($value as $key1=>$value1) {
+                <h2>{{$key1}}</h2>
+                {{$value1}}
+                     <BR>
+
+
+    @endforeach
+    @endforeach
+
+
+
+</body>
+</html>
